565738143789e557984cf122f41d8c98b4dad312
mmattel <github@diemattels.at>
2024-07-28 17:50:18 +0200

    Fix envvar error when loading fails

--- solmate_env.py
+++ solmate_env.py
@@ -1,8 +1,8 @@
 import os
 import sys
-from dotenv import dotenv_values
 import json
 import solmate_utils as sol_utils
+from dotenv import dotenv_values
 
 def process_env(version):
 	# get all environment variables as dictionary
@@ -36,6 +36,7 @@ def process_env(version):
 	#print(json.dumps(merged_config, indent=2, sort_keys=True))
 
 	if not ok:
+		sol_utils.merged_config = merged_config
 		sol_utils.logging(message)
 		sys.exit()
 
@@ -89,6 +90,8 @@ def process_env(version):
 		if not mqtt_config:
 			# but there is no mqtt config defined
 			message = 'There is no MQTT configuration, exiting.'
+			# set key to use 'sol_utils.logging' because merged_config has not been populated
+			merged_config['general_console_print'] = True
 			sol_utils.logging(message, merged_config)
 			sys.exit()
 
@@ -98,10 +101,12 @@ def process_env(version):
 
 	if not solmate_config:
 		message = 'There is no Solmate configuration, exiting.'
+		sol_utils.merged_config = merged_config
 		sol_utils.logging(message, merged_config)
 		sys.exit()
 
-	return merged_config
+	# hand over the final array to be globally available
+	sol_utils.merged_config = merged_config
 
 def _add_optional_env(version):
 	# if the key does not exist, it is added with a default value.
